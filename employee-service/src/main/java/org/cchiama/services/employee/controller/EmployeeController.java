f58cb10a9b851e8090e7d51b5e42469a47f66f21
Christian Chiama <beengineering@icloud.com>
2022-11-21 10:57:27 +0100

    initial commit

---
+++ employee-service/src/main/java/org/cchiama/services/employee/controller/EmployeeController.java
@@ -0,0 +1,56 @@
+package org.cchiama.services.employee.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.cchiama.services.employee.model.Employee;
+import org.cchiama.services.employee.repository.EmployeeRepository;
+
+import javax.inject.Inject;
+import javax.validation.Valid;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.Set;
+
+@Path("/employees")
+@Produces(MediaType.APPLICATION_JSON)
+public class EmployeeController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(EmployeeController.class);
+
+    @Inject
+    EmployeeRepository repository;
+
+    @POST
+    public Employee add(@Valid Employee employee) {
+        LOGGER.info("Employee add: {}", employee);
+        return repository.add(employee);
+    }
+
+    @Path("/{id}")
+    @GET
+    public Employee findById(@PathParam("id") Long id) {
+        LOGGER.info("Employee find: id={}", id);
+        return repository.findById(id);
+    }
+
+    @GET
+    public Set<Employee> findAll() {
+        LOGGER.info("Employee find");
+        return repository.findAll();
+    }
+
+    @Path("/department/{departmentId}")
+    @GET
+    public Set<Employee> findByDepartment(@PathParam("departmentId") Long departmentId) {
+        LOGGER.info("Employee find: departmentId={}", departmentId);
+        return repository.findByDepartment(departmentId);
+    }
+
+    @Path("/organization/{organizationId}")
+    @GET
+    public Set<Employee> findByOrganization(@PathParam("organizationId") Long organizationId) {
+        LOGGER.info("Employee find: organizationId={}", organizationId);
+        return repository.findByOrganization(organizationId);
+    }
+
+}
---
+++ organization-service/src/test/java/org/cchiama/services/organization/client/MockDepartmentClient.java
@@ -0,0 +1,26 @@
+package org.cchiama.services.organization.client;
+
+import io.quarkus.test.Mock;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.cchiama.services.organization.model.Department;
+
+import javax.enterprise.context.ApplicationScoped;
+import java.util.Collections;
+import java.util.List;
+
+@Mock
+@ApplicationScoped
+@RestClient
+public class MockDepartmentClient implements DepartmentClient {
+
+    @Override
+    public List<Department> findByOrganization(Long organizationId) {
+        return Collections.singletonList(new Department("Test1"));
+    }
+
+    @Override
+    public List<Department> findByOrganizationWithEmployees(Long organizationId) {
+        return null;
+    }
+
+}
